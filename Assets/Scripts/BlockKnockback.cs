89803957e2b0f9ae142917f06f5e071cc7f18da2
adelkesc <256523@via.dk>
2020-05-30 15:35:31 +0200

    Added knockback and killzone scripts.

---
+++ Assets/Scripts/BlockKnockback.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockKnockback : MonoBehaviour //IInteractable
+{
+    public float knockForce;
+    private GameObject player;
+    private Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRB = player.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerRB.AddForce(playerRB.velocity * -knockForce, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/KillzoneMain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillzoneMain : MonoBehaviour
+{
+    public GameObject killBoundary;
+    public float moveSpeed;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(PauseForPlayer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The plane is rotated so it uses the Y axis to move in the Z axis.
+        killBoundary.transform.Translate(new Vector3(0, moveSpeed, 0) * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("The player will die here!");
+            //Trigger player death, killzone stops moving
+            //player.SetActive(false);
+            moveSpeed = 0;
+        }
+    }
+    IEnumerator PauseForPlayer()
+    {
+        Debug.Log("PAUSE");
+        yield return new WaitForSeconds(50f);
+    }
+}
